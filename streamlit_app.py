40b4deb7132b759ecc7cf2f8aa860366c70b445a
Shamela-K <shamelakumar@gmail.com>
2024-06-14 19:03:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,48 @@
 import numpy as np
 import pandas as pd
-# import joblib
+import joblib
 import streamlit as st 
 from PIL import Image
 
-# model = joblib.load('classifier.joblib')
+model = joblib.load('classifier.joblib')
 
 image = Image.open('water.jpg')
 st.image(image.resize((1000, 300)))
 
-def predict_water_potability(ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic_carbon, Trihalomethanes, Turbidity):
-    prediction = model.predict([[ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic_carbon, Trihalomethanes, Turbidity]])
+def predict_water_potability(ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity):
+    prediction = model.predict([[ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity]])
     return prediction
 
 def main():
     st.title("Water Potability Web APP")
-    ph			    = st.text_input("ph", placeholder="Type Here") 
-    Hardness		= st.text_input("Hardness", placeholder="Type Here")
-    Solids			= st.text_input("Solids", placeholder="Type Here")
-    Chloramines		= st.text_input("Chloramines", placeholder="Type Here")
-    Sulfate			= st.text_input("Sulfate", placeholder="Type Here")
-    Conductivity	= st.text_input("Conductivity", placeholder="Type Here")	
-    Organic_carbon	= st.text_input("Organic_carbon", placeholder="Type Here")	
-    Trihalomethanes	= st.text_input("Trihalomethanes", placeholder="Type Here")	
-    Turbidity		= st.text_input("Turbidity", placeholder="Type Here")
+    ph = st.text_input("ph", placeholder="Type Here") 
+    hardness = st.text_input("Hardness", placeholder="Type Here")
+    solids = st.text_input("Solids", placeholder="Type Here")
+    chloramines = st.text_input("Chloramines", placeholder="Type Here")
+    sulfate = st.text_input("Sulfate", placeholder="Type Here")
+    conductivity = st.text_input("Conductivity", placeholder="Type Here")	
+    organic_carbon = st.text_input("Organic_carbon", placeholder="Type Here")	
+    trihalomethanes = st.text_input("Trihalomethanes", placeholder="Type Here")	
+    turbidity = st.text_input("Turbidity", placeholder="Type Here")
 
     if st.button("Get Prediction"):
-        output = predict_water_potability(ph, Hardness, Solids, Chloramines, Sulfate, Conductivity, Organic_carbon, Trihalomethanes, Turbidity)
-        st.success(f'Result: {output}.')
-        st.write('0 = Water is not Potable (water is not safe for human consumption)')
-        st.write('1 = Water is Potable (water is safe for human consumption)')
+        output = predict_water_potability(ph, hardness, solids, chloramines, sulfate, conductivity, organic_carbon, trihalomethanes, turbidity)
+        st.session_state['prediction'] = output
 
+    if 'prediction' in st.session_state:
+        if st.session_state['prediction'] == 0:
+            st.markdown("<h3>Result :<span style='color:red'> 0 </span></h3>", unsafe_allow_html=True)
+        else:
+            st.markdown("<h3>Result :<span style='color:LawnGreen'> 1 </span></h3>", unsafe_allow_html=True)
+            
+        st.markdown("<h5 style='color:red'> 0<span style='color:white'> =</span> Water is Not Potable (not safe for human consumption)</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='color:lawngreen'> 1<span  style='color:White'> =</span> Water is Potable (safe for human consumption)</h5>", unsafe_allow_html=True)
+        
     if st.button("About"):
         st.text("Classifier name : Random Forest")
-        st.text("Accuracy Score : 70.00")
+        st.text("Accuracy Score : 68.00")
+        st.text("Built by : Shamela K")
+
 
 if __name__ == '__main__':
     main()
